fa0914bc8ad44cbd9566110507c8dbb46d6a648d
fsystemweb <facu.cappella@gmail.com>
2025-11-15 18:44:14 +0100

    fix lint

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,5 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { provideZonelessChangeDetection } from '@angular/core';
